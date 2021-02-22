98cb45a5deca1bd8401e69334b5d73473dd26f66
Harrison <harrisonhuang00@gmail.com>
2021-02-21 16:38:30 -0800

    Add data into RMP card

--- frontend/src/components/rmp/RMP.js
+++ frontend/src/components/rmp/RMP.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react'
-import { Grid, Box, } from 'grommet'
-import { RMPSidebar } from './RMPSidebar'
-import { RMPReviews } from './RMPReviews'
+import React, { useState } from "react";
+import { Grid, Box } from "grommet";
+import { RMPSidebar } from "./RMPSidebar";
+import { RMPReviews } from "./RMPReviews";
 
-export const RMP = () => {
-    const [selectedInstructor, setSelectedInstructor] = useState(null)
+export const RMP = ({ course }) => {
+  const [selectedInstructor, setSelectedInstructor] = useState(null);
 
+  return (
+    // TODO responsive Grid
+    <Grid
+      rows={["xxsmall", "flex"]}
+      columns={["300px", "flex"]}
+      areas={[
+        { name: "Header", start: [0, 0], end: [1, 0] },
+        { name: "Sidebar", start: [0, 1], end: [0, 1] },
+        { name: "Content", start: [1, 1], end: [1, 1] },
+      ]}
+      fill="vertical"
+    >
+      <Box
+        background="light-5"
+        gridArea="Header"
+        round={{ size: "small", corner: "top" }}
+      >
+        Header
+      </Box>
 
-    return (
-        // TODO responsive Grid
-        <Grid
-            rows={['xxsmall', 'flex']}
-            columns={['300px', 'flex']}
-            areas={[
-                { name: 'Header', start: [0, 0], end: [1, 0] },
-                { name: 'Sidebar', start: [0, 1], end: [0, 1] },
-                { name: 'Content', start: [1, 1], end: [1, 1] }
-            ]}
-            fill="vertical"
-        >
-            <Box background="light-5" gridArea="Header" round={{ size: "small", corner: "top" }}>
-                Header
-            </Box>
+      <RMPSidebar
+        gridArea="Sidebar"
+        course={course}
+        selectedInstructor={selectedInstructor}
+        setSelectedInstructor={setSelectedInstructor}
+      />
 
-            <RMPSidebar gridArea="Sidebar" selectedInstructor={selectedInstructor} setSelectedInstructor={setSelectedInstructor} />
-
-            <RMPReviews gridArea="Content" selectedInstructor={selectedInstructor} />
-        </Grid>
-    )
-}
+      <RMPReviews
+        gridArea="Content"
+        course={course}
+        selectedInstructor={selectedInstructor}
+      />
+    </Grid>
+  );
+};
